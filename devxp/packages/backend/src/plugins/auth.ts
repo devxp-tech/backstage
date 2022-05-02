1258cfedf94806a5da4f706cd61b38d5b4d4b926
Diego Luisi <diego.luisi@cea.com.br>
2022-05-02 15:26:57 +0100

    [template] add golang template

---
+++ devxp/packages/backend/src/plugins/auth.ts
@@ -0,0 +1,15 @@
+import { createRouter } from '@backstage/plugin-auth-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
+  return await createRouter({
+    logger: env.logger,
+    config: env.config,
+    database: env.database,
+    discovery: env.discovery,
+    tokenManager: env.tokenManager,
+  });
+}
