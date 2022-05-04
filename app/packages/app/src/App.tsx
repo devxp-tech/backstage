73d600289cb272477af544c3db803bd08263c406
Diego Luisi <diego.luisi@cea.com.br>
2022-05-04 21:09:23 +0100

    [template] add golang template

--- app/packages/app/src/App.tsx
+++ app/packages/app/src/App.tsx
@@ -32,25 +32,27 @@ import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { PermissionedRoute } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 
-import { githubAuthApiRef } from '@backstage/core-plugin-api';
-import { SignInPage } from '@backstage/core-components';
+//import { githubAuthApiRef } from '@backstage/core-plugin-api';
+//import { SignInProviderConfig, SignInPage } from '@backstage/core-components';
+//
+//const githubProvider: SignInProviderConfig = {
+//  id: 'github-auth-provider',
+//  title: 'GitHub',
+//  message: 'Sign in using GitHub',
+//  apiRef: githubAuthApiRef,
+//};
 
 const app = createApp({
+  //components: {
+  //  SignInPage: props => (
+  //    <SignInPage
+  //      {...props}
+  //      auto
+  //      provider={githubProvider}
+  //    />
+  //  ),
+  //},
   apis,
-    components: {
-        SignInPage: props => (
-          <SignInPage
-            {...props}
-            auto
-            provider={{
-            id: 'github',
-            title: 'GitHub',
-            message: 'Sign in using GitHub',
-            apiRef: githubAuthApiRef,
-          }}
-        />
-      ),
-    },
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
