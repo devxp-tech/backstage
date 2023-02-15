48c576097c77104abb5229897a7e4bf6b8437f8d
Diego Luisi <dluisi@evolution.com>
2023-02-15 18:48:06 +0000

    [BUMP] :up: Upgrade Apps

--- app/packages/backend/src/plugins/catalog.ts
+++ app/packages/backend/src/plugins/catalog.ts
@@ -2,7 +2,8 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { GitHubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+// import { GitHubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import { GithubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 
 
 export default async function createPlugin(
@@ -13,7 +14,7 @@ export default async function createPlugin(
   // The org URL below needs to match a configured integrations.github entry
   // specified in your app-config.
   builder.addEntityProvider(
-    GitHubOrgEntityProvider.fromConfig(env.config, {
+    GithubOrgEntityProvider.fromConfig(env.config, {
       id: 'production',
       orgUrl: 'https://github.com/devxp-tech',
       logger: env.logger,
