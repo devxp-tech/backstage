f064d429519b768a076911958d27cc901b0c3449
Diego Luisi <dluisi@evolution.com>
2023-02-15 12:31:21 +0000

    [BUMP] :up: Upgrade Apps

--- app/packages/backend/src/plugins/catalog.ts
+++ app/packages/backend/src/plugins/catalog.ts
@@ -1,12 +1,8 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
-
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-
-import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
-
-
+import { GitHubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 
 
 export default async function createPlugin(
@@ -16,9 +12,8 @@ export default async function createPlugin(
 
   // The org URL below needs to match a configured integrations.github entry
   // specified in your app-config.
-
   builder.addEntityProvider(
-    GithubEntityProvider.fromConfig(env.config, {
+    GitHubOrgEntityProvider.fromConfig(env.config, {
       id: 'production',
       orgUrl: 'https://github.com/devxp-tech',
       logger: env.logger,
@@ -29,10 +24,7 @@ export default async function createPlugin(
     }),
   );
 
-
-
   builder.addProcessor(new ScaffolderEntitiesProcessor());
-
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
