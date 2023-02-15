54ab49b5a2c4389bd27c0ec6b5f4c15c3432062e
Diego Luisi <dluisi@evolution.com>
2023-02-15 12:01:10 +0000

    [BUMP] :up: Upgrade Apps

--- app/packages/backend/src/plugins/catalog.ts
+++ app/packages/backend/src/plugins/catalog.ts
@@ -1,12 +1,12 @@
-// import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
-import { CatalogBuilder, EntityProvider } from '@backstage/plugin-catalog-backend';
+import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-// import { GitHubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 
-import { ScmIntegrations, DefaultGithubCredentialsProvider } from '@backstage/integration';
-// import { GitHubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+
+
 
 
 export default async function createPlugin(
@@ -17,35 +17,23 @@ export default async function createPlugin(
   // The org URL below needs to match a configured integrations.github entry
   // specified in your app-config.
 
-  // builder.addEntityProvider(
-  //   GitHubOrgEntityProvider.fromConfig(env.config, {
-  //     id: 'production',
-  //     orgUrl: 'https://github.com/devxp-tech',
-  //     logger: env.logger,
-  //     schedule: env.scheduler.createScheduledTaskRunner({
-  //       frequency: { minutes: 60 },
-  //       timeout: { minutes: 15 },
-  //     }),
-  //   }),
-  // );
+  builder.addEntityProvider(
+    GithubEntityProvider.fromConfig(env.config, {
+      id: 'production',
+      orgUrl: 'https://github.com/devxp-tech',
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 60 },
+        timeout: { minutes: 15 },
+      }),
+    }),
+  );
 
-  const integrations = ScmIntegrations.fromConfig(env.config);
 
-  const githubCredentialsProvider = DefaultGithubCredentialsProvider.fromIntegrations(integrations);
 
-  const gitProvider = GitHubOrgEntityProvider.fromConfig(env.config, {
-    id: "github-org-entity-provider",
-    orgUrl: "https://github.com/devxp-tech", // 🚨 REPLACE
-    logger: env.logger,
-    githubCredentialsProvider
-  });
-
-  builder.addEntityProvider(gitProvider as EntityProvider);
-
-  // builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addProcessor(new ScaffolderEntitiesProcessor());
 
   const { processingEngine, router } = await builder.build();
-  // await processingEngine.start();
-  await gitProvider.read();
+  await processingEngine.start();
   return router;
 }
