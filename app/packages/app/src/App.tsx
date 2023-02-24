d1e6daa0866ce3b454fca273d159da4697d86b09
Diego Luisi <dluisi@evolution.com>
2023-02-24 20:56:16 +0000

    :zap: Default Commit

--- app/packages/app/src/App.tsx
+++ app/packages/app/src/App.tsx
@@ -23,8 +23,6 @@ import {
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 
-import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
-import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
 import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
